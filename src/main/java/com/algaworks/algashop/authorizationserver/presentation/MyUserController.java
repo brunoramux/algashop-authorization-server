6b61b33f6c483217f10cfa2b65ff6f26fef9dcf3
Bruno Ramos Lemos <bruno.lemos@live.com>
2026-07-15 19:35:25 -0300

    feat: Checking if AuthUser Authenticated

--- src/main/java/com/algaworks/algashop/authorizationserver/presentation/MyUserController.java
+++ src/main/java/com/algaworks/algashop/authorizationserver/presentation/MyUserController.java
@@ -1,16 +1,28 @@
 package com.algaworks.algashop.authorizationserver.presentation;
 
+import com.algaworks.algashop.authorizationserver.application.security.SecurityCheckApplicationService;
+import com.algaworks.algashop.authorizationserver.application.user.query.AuthUserOutput;
+import com.algaworks.algashop.authorizationserver.application.user.query.AuthUserQueryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/api/v1/users/me")
+@RequiredArgsConstructor
 public class MyUserController {
 
-    @GetMapping
-    public void getMe(){
+    private final SecurityCheckApplicationService securityCheckApplicationService;
+    private final AuthUserQueryService authUserQueryService;
 
+    @GetMapping
+    public AuthUserOutput getMe(){
+        UUID authenticatedUserId = securityCheckApplicationService.getAuthenticatedUserId();
+
+        return authUserQueryService.findById(authenticatedUserId);
     }
 
 }
